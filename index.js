39ba06b58af68de797d065ebd868f5e63629c698
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-11-02 20:20:37 +0530

    Cart stage 1

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 import { menuArray } from "./data.js"
 
 const rootEl = document.getElementById("root")
+const cart = []
+const totalPriceEl = document.getElementById("total-price")
+const cartElContainer = document.getElementById("cart-items-container")
+
+rootEl.addEventListener("click", (event) => {
+    if (event.target.dataset.item) {
+        handleAddItem(event.target.dataset.item)
+    }
+})
+
+function handleAddItem(itemAdded){
+    const itemAddedIndex = menuArray.findIndex((item) => item.id == itemAdded)
+    const cartItemobject = {
+        id: itemAdded,
+        name: menuArray[itemAddedIndex].name,
+        price: menuArray[itemAddedIndex].price,
+        quantity: 1,
+    }
+
+    let cartArrayBoolean = false
+    
+    cart.forEach((item) => {
+        if (item.name == cartItemobject.name){
+            cartArrayBoolean = true
+        }
+    })
+    if (cartArrayBoolean) {
+        cart[menuArray.findIndex((item) => item.name == cartItemobject.name)].quantity ++
+    } else {
+        cart.push(cartItemobject)
+    }
+}
+
+function generateCartHTML(){
+    let cartHTML = "";
+    cart.forEach((cartItem) => {
+        cartHTML += `
+        <div id="cart-items-container">
+            <div class="cart-item">
+                <h2 class="item-name-and-qty">${cartItem.name} X ${cartItem.quantity}</h2>
+                <button class="remove-from-cart" data-id="${cartItem.id}">remove</button>
+                <h2 class="item-total">${cartItem.price*cartItem.quantity}$</h2>
+            </div>
+        </div>
+        `
+    })
+      return cartHTML;         
+}
 
 function generateMenuHTML (){
     let menuHTML = ""
@@ -21,6 +69,12 @@ function generateMenuHTML (){
 
 function render () {
     rootEl.innerHTML = generateMenuHTML()
+    if (cart.length > 0) {
+        cartElContainer.innerHTML = (generateCartHTML())
+    } else {
+        cartElContainer.innerHTML = ""
+    }
+    
 }
 
 render()
